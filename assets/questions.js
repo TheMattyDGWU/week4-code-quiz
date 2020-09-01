d1e93015168173f867ef864898bbc22a1c786f48
TheMattyDGWU <themattydgwu@gmail.com>
2020-08-31 23:44:12 -0400

    initial

---
+++ assets/questions.js
@@ -0,0 +1,32 @@
+var jsQuestions = [
+  {
+    title: "Which tag is used to mark the beginning of a paragraph?",
+    choices: ["<td>", "<br>", "<p>", "<tr>"],
+    answer: "<p>"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "HTML stands for _____?",
+    choices: ["Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "None of these"],
+    answer: "Hyper Text Markup Language"
+  },
+  {
+    title: "DOM is an abreviation for ____",
+    choices: ["Data Object Mode", "Dude On Meds", "Document Object Model", "Data Object Modality"],
+    answer: "Document Object Model"
+  },
+  {
+    title: "Which of the following bootstrap style options is used to add text to .navbar?",
+    choices: [".navbar-text", ".text", ".form-text", "None of the above"],
+    answer: ".navbar-text"
+  },
+  {
+    title: "The three foundational languages of the web are HTML, CSS, and CoffeeBeans!",
+    choices: ["True", "False"],
+    answer: "False"
+  }
+];

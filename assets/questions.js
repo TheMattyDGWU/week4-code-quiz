4a862af3e2c193635698e2db00c0bba371cef41d
TheMattyDGWU <themattydgwu@gmail.com>
2020-09-09 18:21:38 -0400

    made everything PRETTIER

--- assets/questions.js
+++ assets/questions.js
@@ -3,26 +3,33 @@ var jsQuestions = [
   {
     title: "Which tag is used to mark the beginning of a paragraph?",
     choices: ["<td>", "<br>", "<p>", "<tr>"],
-    answer: "<p>"
+    answer: "<p>",
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    answer: "parentheses",
   },
   {
-    title: "DOM is an abreviation for ____",
-    choices: ["Data Object Mode", "Dude On Meds", "Document Object Model", "Data Object Modality"],
-    answer: "Document Object Model"
+    title: "DOM is an abbreviation for ____",
+    choices: [
+      "Data Object Mode",
+      "Dude On Meds",
+      "Document Object Model",
+      "Data Object Modality",
+    ],
+    answer: "Document Object Model",
   },
   {
-    title: "Which of the following bootstrap style options is used to add text to .navbar?",
+    title:
+      "Which of the following bootstrap style options is used to add text to .navbar?",
     choices: [".navbar-text", ".text", ".form-text", "None of the above"],
-    answer: ".navbar-text"
+    answer: ".navbar-text",
   },
   {
-    title: "True or False: 'let' creates a local variable, while 'var' creates a global variable.",
+    title:
+      "True or False: 'let' creates a local variable, while 'var' creates a global variable.",
     choices: ["True", "False"],
-    answer: "True"
-  }
+    answer: "True",
+  },
 ];
